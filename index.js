882867a0d36cc49a47dd7348488ad7cc133667e4
aashishbiradar <aashish.biradar@gmail.com>
2020-11-14 01:15:34 +0530

    live code share

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
-var http = require('http').createServer(app);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('codeChange', (payload) => {
+    console.log('codeChange:\n' + payload);
+    socket.broadcast.emit('codeChange', payload);
+  });
 });
 
 http.listen(3000, () => {
