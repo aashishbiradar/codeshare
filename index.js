970c07b3486bb5a62e5e3553d294eed8e3b26d00
aashishbiradar <aashish.biradar@gmail.com>
2020-11-17 14:09:50 +0530

    heroku setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
@@ -16,6 +17,4 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+http.listen(port, () => console.log(`listening on port ${port}`));
