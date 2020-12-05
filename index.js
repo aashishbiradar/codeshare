d54bcdb215d78297be9353dad65aa66b5adcd67f
aashishbiradar <aashish.biradar@gmail.com>
2020-12-05 23:27:44 +0530

    point all reqs to index.html

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
+
+const port = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.static('public'));
+app.get('/*', (req,res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
@@ -10,8 +18,6 @@ const io = require('socket.io')(http, {
     methods: ["GET", "POST"],
   }
 });
-
-const port = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
   socket.on('join', ({ shareId }) => {
